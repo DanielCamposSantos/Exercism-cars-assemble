ed21bd12d5fbc54d27d2896df96cd171d7f162ef
DanielCamposSantos <danielcampos.santos1603@gmail.com>
2025-05-05 23:02:10 -0300

    First commit

--- src/main/java/CarsAssemble.java
+++ src/main/java/CarsAssemble.java
@@ -1,10 +1,21 @@
 public class CarsAssemble {
 
     public double productionRatePerHour(int speed) {
-        throw new UnsupportedOperationException("Please implement the CarsAssemble.productionRatePerHour() method");
+        double succesRate = 0;
+        double production = speed * 221;
+        if (speed >= 1 && speed <= 4){
+            succesRate = 1;
+        } else if (speed >= 5 && speed <= 8){
+            succesRate = 0.9;
+        }else if (speed == 9 ){
+            succesRate = 0.8;
+        }else if (speed == 10){
+            succesRate = 0.77;
+        }
+        return  production * succesRate;
     }
 
     public int workingItemsPerMinute(int speed) {
-        throw new UnsupportedOperationException("Please implement the CarsAssemble.workingItemsPerMinute() method");
+        return (int) productionRatePerHour(speed) / 60;
     }
 }
